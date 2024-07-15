7b33f6ce14e6843abef07723b4df6f52292bbddd
izancode <sf48116@gmail.com>
2024-07-15 21:48:42 +0530

    final

--- App.js
+++ App.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hey I am h1 tag"),
+    React.createElement("h2", {}, "Hey I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hey I am h1 tag"),
+    React.createElement("h2", {}, "Hey I am h2 tag"),
+  ]),
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
