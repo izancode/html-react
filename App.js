43ffbd79b5535c0fe2bc4f671140717ba8f2c6a9
izancode <sf48116@gmail.com>
2024-07-16 19:47:47 +0530

    last code of  pro_02

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hey I am h1 tag"),
-    React.createElement("h2", {}, "Hey I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hey I am h1 tag"),
-    React.createElement("h2", {}, "Hey I am h2 tag"),
-  ]),
-]);
-console.log(parent);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello my name is faizan React"
+);
+
+console.log(heading);
+
+/* JSX - is not HTML in JS but its html or xml like syntax
+JSX transpiled before it reaches the JS - parcel - Babel
+
+JSX converted to React.createElement and this converted to ReactElement(object) and this converted to html element
+*/
+
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    hello my name is faizan Jsx
+  </h1>
+);
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
+console.log(root);
